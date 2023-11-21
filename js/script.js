9a5db499eba24566fe992b8c2abf2352627fb4b9
Sahak <sahak.keshishyan.dev@gmail.com>
2023-11-21 04:33:19 +0000

    mobile navigation, implementing smooth scrolling (for safary in progress)

---
+++ js/script.js
@@ -0,0 +1,10 @@
+const year = document.querySelector(".year");
+const currentYear = new Date().getFullYear();
+year.textContent = currentYear;
+
+("nav-open");
+const btnNav = document.querySelector(".btn-mobile-nav");
+const header = document.querySelector(".header");
+btnNav.addEventListener("click", () => {
+  header.classList.toggle("nav-open");
+});
